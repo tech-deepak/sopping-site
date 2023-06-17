c7e02b85c4b5bafa58abc9aa3e02014c1522fdee
tech-deepak <deepakkumarcopy@gmail.com>
2023-06-17 11:07:30 +0530

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,6 +11,7 @@ import { ApiService } from '../api.service';
 
 export class LoginComponent implements OnInit {
 
+	loginForm: any;
 	constructor(
 		public apiService: ApiService,
 		public activatedRoute: ActivatedRoute
@@ -20,12 +21,12 @@ export class LoginComponent implements OnInit {
 	}
 
 	login(data: any) {
-		console.log(data)
+		console.log(data.form.value)
 		let endpoint = ApiCallback.LOGIN;
 		if(location.pathname === '/admin/login') {
 			endpoint = ApiCallback.LOGIN_ADMIN
 		}
-		this.apiService.post(endpoint, data).subscribe(response => {
+		this.apiService.post(endpoint, data.form.value).subscribe(response => {
 			
 		})
 	}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiCallback } from '../api-callback';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,13 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class SignupComponent implements OnInit {
 
   signupForm: any;
-  constructor() { }
+  constructor(
+    public apiService: ApiService
+  ) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(data: any){
     console.log("1111111111111", data)
+    this.apiService.post(ApiCallback.SING_UP, data.form.value).subscribe((res) => {
+      
+    })
   }
 
 } 
